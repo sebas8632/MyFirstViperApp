4ae045029eac9d6c272fb95d405e69f69dfba6c2
sebastian <sebastian8632@gmail.com>
2017-12-03 19:50:34 -0500

    Has been added the protocol file

--- MyFirstViperApp/PostListProtocol.swift
+++ MyFirstViperApp/PostListProtocol.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+import UIKit
+
+protocol PostListViewProtocol: class {
+    var presenter: PostListPresenterProtocol? { get set }
+    
+    // PRESENTER -> VIEW
+    func showPosts(with posts: [PostModel])
+    
+    func showError()
+    
+    func showLoading()
+    
+    func hideLoading()
+}
+
+protocol PostListPresenterProtocol: class {
+    var view: PostListViewProtocol? { get set }
+    var interactor: PostListInteractorInputProtocol? { get set }
+    var wireFrame: PostListWireFrameProtocol? { get set }
+    
+    // VIEW -> PRESENTER
+    func viewDidLoad()
+    func showPostDetail(forPost post: PostModel)
+}
+
+protocol PostListInteractorInputProtocol: class {
+    var presenter: PostListInteractorOutputProtocol? { get set }
+   // var localDatamanager: PostListLocalDataManagerInputProtocol? { get set }
+    var remoteDatamanager: PostListRemoteDataManagerInputProtocol? { get set }
+    
+    // PRESENTER -> INTERACTOR
+    func retrievePostList()
+}
+
+protocol PostListWireFrameProtocol: class {
+    static func createPostListModule() -> UIViewController
+    // PRESENTER -> WIREFRAME
+    func presentPostDetailScreen(from view: PostListViewProtocol, forPost post: PostModel)
+}
+
+protocol PostListInteractorOutputProtocol: class {
+    // INTERACTOR -> PRESENTER
+    func didRetrievePosts(_ posts: [PostModel])
+    func onError()
+}
+
+protocol PostListRemoteDataManagerInputProtocol: class {
+    var remoteRequestHandler: PostListRemoteDataManagerOutputProtocol? { get set }
+    
+    // INTERACTOR -> REMOTEDATAMANAGER
+    func retrievePostList()
+}
+
+protocol PostListRemoteDataManagerOutputProtocol: class {
+    // REMOTEDATAMANAGER -> INTERACTOR
+    func onPostsRetrieved(_ posts: [PostModel])
+    func onError()
+}
+
+
